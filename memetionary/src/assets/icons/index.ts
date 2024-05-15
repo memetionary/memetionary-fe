0a00036975743754a20ba30d676a92e3288c5dd2
Hemudi <ksum1205@naver.com>
2024-05-15 13:41:30 +0900

    :lipstick: Svgr 을 이용한 주요 Icon 컴포넌트 생성

---
+++ memetionary/src/assets/icons/index.ts
@@ -0,0 +1,8 @@
+export { default as Bad } from './Bad';
+export { default as Good } from './Good';
+export { default as Logo } from './Logo';
+export { default as MemeColor } from './MemeColor';
+export { default as Meme } from './Meme';
+export { default as Opinion } from './Opinion';
+export { default as Random } from './Random';
+export { default as Search } from './Search';
