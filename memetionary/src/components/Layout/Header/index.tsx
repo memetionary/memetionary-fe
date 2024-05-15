2ac8bd3e8f5e08e2bbbd1e6b5f85edb92018975f
Hemudi <ksum1205@naver.com>
2024-05-15 16:49:24 +0900

    :sparkles: Header Logo에 메인페이지 이동 링크 추가

--- memetionary/src/components/Layout/Header/index.tsx
+++ memetionary/src/components/Layout/Header/index.tsx
@@ -6,10 +6,12 @@ const TITLE_TEXT = 'Memetionary';
 const Header = () => {
   return (
     <header className="flex h-14 w-full select-none items-center justify-between border-b-2 px-3">
-      <div className="flex items-center gap-2">
-        <IconLogo />
-        <h1 className="text-lg">{TITLE_TEXT}</h1>
-      </div>
+      <Link href={'/'}>
+        <div className="flex items-center gap-2">
+          <IconLogo />
+          <h1 className="text-lg">{TITLE_TEXT}</h1>
+        </div>
+      </Link>
       <div className="flex items-center gap-3">
         <Link href={'/'}>
           <IconRandom />
