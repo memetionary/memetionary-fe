6e253815032dc04361b42ee68a517b7b08eebf87
연 <mogoo22@naver.com>
2024-05-24 07:20:19 +0900

    ✨ gamil 메일링 기능

--- memetionary/src/app/contact/page.tsx
+++ memetionary/src/app/contact/page.tsx
@@ -1,40 +1,92 @@
 'use client';
 
-import { useState } from 'react';
+import { ChangeEvent, MouseEventHandler, useState } from 'react';
 import Input from '@/components/Input';
 import Select from '@/components/Select';
 import TextArea from '@/components/TextArea';
 import { CONTACT_OPTIONS } from '@/components/Select/constants';
+import { contactMail } from '@/api/contact';
+import { validateContactForm } from '@/utils/validator/contactForm';
 
-interface Form {
+export interface ContactForm {
   option: string;
   email: string;
   title: string;
   content: string;
 }
 
+const INIT_FORM: ContactForm = {
+  option: '',
+  email: '',
+  title: '',
+  content: '',
+};
+
 const borderStyle = 'border border-solid border-gray-200 rounded-lg p-4 focus:outline-none';
 
 export default function Contact() {
-  const [form, setForm] = useState<Form>();
+  const [form, setForm] = useState<ContactForm>(INIT_FORM);
+
+  const handleChangeForm = (e: ChangeEvent) => {
+    const target = e.target as HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement;
+    setForm((prev) => ({ ...prev, [target.id]: target.value }));
+  };
+
+  const handleClickSubmit: MouseEventHandler<HTMLButtonElement> = async (e) => {
+    e.preventDefault();
+    const inValid = validateContactForm(form);
+    if (inValid) {
+      alert(inValid);
+      return;
+    }
+    await contactMail({ form });
+    const SUCCESS_SUBMIT_MSG = '성공적으로 문의가 접수되었습니다.';
+    alert(SUCCESS_SUBMIT_MSG);
+    setForm(INIT_FORM);
+  };
 
   return (
-    <div className="grid h-screen w-full content-around p-8">
-      <h2 className="text-3xl font-bold">{'이메일 문의하기'}</h2>
+    <>
       <div className="grid gap-y-6">
-        <Select className={borderStyle} options={CONTACT_OPTIONS} value={form?.option} />
-        <Input className={borderStyle} type="email" placeholder="이메일" value={form?.email} />
-        <Input className={borderStyle} type="text" placeholder="제목" value={form?.title} />
+        <Select
+          id="option"
+          className={borderStyle}
+          options={CONTACT_OPTIONS}
+          value={form?.option}
+          onChange={handleChangeForm}
+        />
+        <Input
+          id="email"
+          className={borderStyle}
+          type="email"
+          placeholder="이메일"
+          value={form?.email}
+          onChange={handleChangeForm}
+        />
+        <Input
+          id="title"
+          className={borderStyle}
+          type="text"
+          placeholder="제목"
+          value={form?.title}
+          onChange={handleChangeForm}
+        />
         <TextArea
+          id="content"
           className={`${borderStyle} h-52`}
-          placeholder="문의 내용"
+          placeholder="문의 내용 (10자 이상 입력해주세요.)"
           value={form?.content}
           showTextLength={true}
+          onChange={handleChangeForm}
         />
       </div>
-      <button className="w-full justify-self-center rounded-lg bg-violet-800 p-6 font-bold text-white">
+      <button
+        type="submit"
+        className="w-full justify-self-center rounded-lg bg-primary-700  p-6 font-bold text-white disabled:bg-primary-100"
+        onClick={handleClickSubmit}
+      >
         {'문의접수'}
       </button>
-    </div>
+    </>
   );
 }
