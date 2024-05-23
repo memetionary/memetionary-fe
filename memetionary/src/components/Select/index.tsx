e5ee6d6e7416c9f679d7ffca26d5eaf13d9a3b47
연 <mogoo22@naver.com>
2024-05-24 07:00:32 +0900

    ✨ 컴포넌트 props value와 state 연동

--- memetionary/src/components/Select/index.tsx
+++ memetionary/src/components/Select/index.tsx
@@ -5,13 +5,18 @@ interface SelectProps extends DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectE
 }
 
 export default function Select({ options, ...props }: SelectProps) {
-  const [value, setValue] = useState(options[0]);
+  const optionType = options[0];
+  const [value, setValue] = useState(optionType);
 
   const handleChangeSelect: ChangeEventHandler<HTMLSelectElement> = (e) => {
     const { value: newValue } = e.target;
     setValue(newValue);
     props?.onChange?.(e);
   };
+
+  useEffect(() => {
+    setValue(String(props.value || optionType));
+  }, [props.value]);
 
   return (
     <select {...props} value={value} onChange={handleChangeSelect}>
