0c96fd80d12440fa60abe926ccd537d2f6a512d3
Hemudi <ksum1205@naver.com>
2024-05-24 22:11:06 +0900

    :sparkles: Icon 컴포넌트 추가

---
+++ memetionary/src/assets/icons/IconLeftDirection.tsx
@@ -0,0 +1,7 @@
+import type { SVGProps } from 'react';
+const SvgIconLeftDirection = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" {...props}>
+    <path d="M372.8 1.6c-6.8 1.6-12.8 4.5-18.3 8.8C327.9 31.1 92.4 221.9 90.2 224.6c-14.9 18-14.9 44.8 0 62.8 3.2 3.9 257.6 209.6 267.3 216.1 19.4 13 44.4 10.6 61-6 3.3-3.3 7.2-8.3 8.7-11.1 6.2-11.5 5.8 3.4 5.8-230.4s.4-218.9-5.8-230.4c-1.5-2.8-5.4-7.9-8.8-11.3-12.2-12.1-28.9-16.8-45.6-12.7" />
+  </svg>
+);
+export default SvgIconLeftDirection;
---
+++ memetionary/src/assets/icons/IconRightDirection.tsx
@@ -0,0 +1,7 @@
+import type { SVGProps } from 'react';
+const SvgIconRightDirection = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" {...props}>
+    <path d="M69 1.6c-13.1 3.5-25.3 12.8-31.2 24C31.6 37.1 32 22.1 32 256c0 233.8-.4 218.9 5.8 230.4 1.5 2.8 5.4 7.9 8.8 11.2 14.7 14.7 37.6 18.5 55.7 9.3 7.6-3.9 351.1-207.1 357-211.2 24.5-17 27.7-52.4 6.8-73.6-4.9-4.9-28.7-19.4-180.6-109.1C189.3 56.1 107.8 8.1 104.5 6.4 93.1.3 80.4-1.4 69 1.6" />
+  </svg>
+);
+export default SvgIconRightDirection;
