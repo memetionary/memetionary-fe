ecc606f8515cb4753b7b379106ea9f840460a946
연 <mogoo22@naver.com>
2024-05-25 17:22:27 +0900

    🩹 API 엔드포인트 수정

--- memetionary/src/api/contact/index.ts
+++ memetionary/src/api/contact/index.ts
@@ -4,7 +4,7 @@ import { ContactForm } from '@/app/contact/page';
 export const contactMail = async ({ form }: { form: ContactForm }) => {
   try {
     // axios.default.baseUrl 합치기
-    await axios.post(`${process.env.NEXT_PUBLIC_URL}/api/contact`, form);
+    await axios.post(`${process.env.NEXT_PUBLIC_URL}/contact`, form);
   } catch (error) {
     throw error;
   }
