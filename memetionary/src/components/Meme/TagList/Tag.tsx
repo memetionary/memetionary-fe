0af6550440da2fb0199dd8e1cc865743594b989e
연 <mogoo22@naver.com>
2024-05-29 17:27:26 +0900

    🎨 컴포넌트 Props를 rest 형태로 수정

--- memetionary/src/components/Meme/TagList/Tag.tsx
+++ memetionary/src/components/Meme/TagList/Tag.tsx
@@ -3,11 +3,13 @@ interface TagProps {
   children: React.ReactNode;
 }
 
-export default function Tag({ isDanger, children }: TagProps) {
+export default function Tag({ ...props }: TagProps) {
   const dangerStyle = 'bg-rose-500 text-white';
   return (
-    <div className={`w-fit rounded-lg border border-solid border-gray-200 p-2 text-xs ${isDanger ? dangerStyle : ''}`}>
-      {children}
+    <div
+      className={`w-fit rounded-lg border border-solid border-gray-200 p-2 text-xs ${props.isDanger ? dangerStyle : ''}`}
+    >
+      {props.children}
     </div>
   );
 }
