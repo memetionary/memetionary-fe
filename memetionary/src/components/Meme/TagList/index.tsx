3aba9815254f5dd9209aff2d6f7054493632d938
연 <mogoo22@naver.com>
2024-05-29 17:27:26 +0900

    🎨 컴포넌트 Props를 rest 형태로 수정

--- memetionary/src/components/Meme/TagList/index.tsx
+++ memetionary/src/components/Meme/TagList/index.tsx
@@ -1,14 +1,16 @@
 import Tag from '@/components/Meme/TagList/Tag';
 
 export interface TagListProps {
-  type: 'info' | 'danger';
-  value: string;
+  list: {
+    type: 'info' | 'danger';
+    value: string;
+  }[];
 }
 
-export default function TagList({ list }: { list: TagListProps[] }) {
+export default function TagList({ ...props }: TagListProps) {
   return (
     <div className="flex flex-wrap gap-1">
-      {list.map((e) => (
+      {props.list.map((e) => (
         <Tag key={e.value} isDanger={e.type === 'danger'}>
           {e.value}
         </Tag>
