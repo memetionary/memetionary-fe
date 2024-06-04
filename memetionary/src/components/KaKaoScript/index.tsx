fbaa85858e982fac3f88b1b0eb1061c4487d5570
Hemudi <ksum1205@naver.com>
2024-06-05 06:41:46 +0900

    :sparkles: Kakao API Script 설정

---
+++ memetionary/src/components/KaKaoScript/index.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import Script from 'next/script';
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
+
+const KAKAO_SCRIPT_URL = 'https://developers.kakao.com/sdk/js/kakao.js';
+const KAKAO_API_KEY = process.env.NEXT_PUBLIC_KAKAO_API_KEY;
+
+const KakaoScript = () => {
+  const onLoad = () => {
+    window.Kakao.init(KAKAO_API_KEY);
+  };
+
+  return <Script src={KAKAO_SCRIPT_URL} async onLoad={onLoad} />;
+};
+
+export default KakaoScript;
