546c54281de7df6b0e8db1c8df55d41652c933e0
연 <mogoo22@naver.com>
2024-06-25 00:25:41 +0900

    ✨ 테스트 API 호출시 로딩 컴포넌트 추가

--- memetionary/src/app/test/layout.tsx
+++ memetionary/src/app/test/layout.tsx
@@ -1,3 +1,6 @@
+import { Suspense } from 'react';
+import Loading from '@/app/test/loading';
+
 const TITLE = {
   CAPTION: '밈셔너리에서 주관하는',
   MAIN: `제 1회 밈능력테스트\n기초 영역`,
@@ -5,12 +8,14 @@ const TITLE = {
 
 export default function TestLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid h-fit min-w-80 max-w-192 content-center gap-4 bg-primary-50 text-center">
-      <div className="px-10 pt-4">
-        <p className="text-sm text-primary-700">{TITLE.CAPTION}</p>
-        <h1 className="whitespace-break-spaces text-2xl font-bold text-primary-700">{TITLE.MAIN}</h1>
+    <Suspense fallback={<Loading />}>
+      <div className="grid max-h-fit min-h-full w-fit grid-rows-[max-content] gap-4 bg-primary-50 text-center">
+        <div className="px-10 pt-4">
+          <p className="text-sm text-primary-700">{TITLE.CAPTION}</p>
+          <h1 className="whitespace-break-spaces text-2xl font-bold text-primary-700">{TITLE.MAIN}</h1>
+        </div>
+        {children}
       </div>
-      {children}
-    </div>
+    </Suspense>
   );
 }
--- memetionary/src/components/Test/Share.tsx
+++ memetionary/src/components/Test/Share.tsx
@@ -8,7 +8,7 @@ interface TestShareProps {
 
 export default function TestShare({ title = SHARE_MSG, ...props }: TestShareProps) {
   return (
-    <div className="grid justify-items-center gap-5 bg-primary-100 px-10 pt-4">
+    <div className="grid w-full justify-items-center gap-5 bg-primary-100 px-10 pt-4">
       <h3 className="text-xl font-bold">{title}</h3>
       <ShareButtonGroups className="w-full rounded-md px-4 py-3" size="lg" />
       <IconStudent width={100} />
