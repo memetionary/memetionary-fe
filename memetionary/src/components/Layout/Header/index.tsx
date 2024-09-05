83de906e9c15d44e5003db76ac91c373780d287b
Hemudi <ksum1205@naver.com>
2024-09-05 15:41:34 +0900

    :lipstick: Header Icon 사이즈 변경

--- memetionary/src/components/Layout/Header/index.tsx
+++ memetionary/src/components/Layout/Header/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import RandomButtom from '@/components/RandomButton';
 import { IconLogo, IconSearch } from '@/assets/icons';
+import RandomButton from '@/components/RandomButton';
 
 const TITLE_TEXT = 'Memetionary';
 
@@ -9,14 +9,14 @@ const Header = () => {
     <header className="flex h-14 w-full select-none items-center justify-between border-b-2 px-3">
       <Link href={'/'}>
         <div className="flex items-center gap-2">
-          <IconLogo width={28} height={28} />
+          <IconLogo width={30} height={30} />
           <h1 className="text-lg">{TITLE_TEXT}</h1>
         </div>
       </Link>
       <div className="flex items-center gap-3">
-        <RandomButtom type="icon" />
+        <RandomButton type="icon" />
         <Link href={'/search'}>
-          <IconSearch width={16} height={16} />
+          <IconSearch width={24} height={24} />
         </Link>
       </div>
     </header>
