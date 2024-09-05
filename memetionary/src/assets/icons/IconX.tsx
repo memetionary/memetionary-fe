eb268c9e739f0c22c5a64f614545a943d5847af5
Hemudi <ksum1205@naver.com>
2024-09-05 16:35:07 +0900

    :lipstick: X 아이콘 변경

--- memetionary/src/assets/icons/IconX.tsx
+++ memetionary/src/assets/icons/IconX.tsx
@@ -1,7 +1,14 @@
 import type { SVGProps } from 'react';
 const SvgIconX = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 384 512" {...props}>
-    <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3l105.4 105.3c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256z" />
+  <svg xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" viewBox="0 0 128 115.7" {...props}>
+    <path
+      d="M100.808 0h19.627l-42.88 49.01L128 115.7H88.502L57.565 75.253 22.167 115.7H2.527l45.866-52.42L0 0h40.501l27.964 36.97Zm-6.89 103.952h10.877L34.592 11.131H22.92Z"
+      style={{
+        fill: '#000',
+        fillOpacity: 1,
+        strokeWidth: 5.93331,
+      }}
+    />
   </svg>
 );
 export default SvgIconX;
