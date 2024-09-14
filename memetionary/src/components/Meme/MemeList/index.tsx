53c605ea3cb76059909e61a1f6e216005f8eba54
연 <mogoo22@naver.com>
2024-09-14 16:25:29 +0900

    🐛 Meme type 변경에 따른 MemeList 컴포넌트 수정

--- memetionary/src/components/Meme/MemeList/index.tsx
+++ memetionary/src/components/Meme/MemeList/index.tsx
@@ -11,16 +11,16 @@ interface MemeListProps {
 const MemeList = ({ memeList, ...props }: MemeListProps) => {
   return (
     <ul className="flex w-full flex-col justify-center gap-5" {...props}>
-      {memeList.map(({ id, title, thumbnail, articles, tags, view, evaluation }) => (
+      {memeList.map(({ id, title, description, tags, view, evaluation }) => (
         <li key={id}>
           <Link href={`/meme/${id}`} className="grid grid-cols-[0.4fr_0.6fr] gap-3">
-            <Thumbnail src={thumbnail} title={title} size="md" />
+            <Thumbnail title={title} size="md" />
             <div className="flex flex-col gap-1">
               <div className="flex w-full justify-between">
                 <h3 className="text-xl font-semibold">{title}</h3>
                 <TitleDetail view={view} evaluation={evaluation} contents={{ title, text: '' }} />
               </div>
-              <p className="w-full">{articles[0]?.content}</p>
+              <p className="w-full">{description}</p>
               <TagList list={tags || []} />
             </div>
           </Link>
